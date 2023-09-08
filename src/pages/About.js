74eef471e085f105d3fab01b3edf082af7f462b5
Vincent Ollivier <olliviev@gmail.com>
2023-09-08 09:35:13 +0200

    Projet terminé

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+import Logo from '../components/Logo';
+
+const About = () => {
+    return (
+        <div>
+            <Logo />
+            <Navigation />
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure officiis optio atque earum illo. Impedit aliquid ab, quia nam exercitationem id quibusdam vel ratione optio qui omnis culpa pariatur recusandae voluptas hic officia sed animi cupiditate obcaecati, repellat consequatur? Porro minima consequatur rem accusantium vero ducimus voluptas nisi officiis obcaecati sapiente alias labore beatae aspernatur reprehenderit exercitationem repudiandae quia unde voluptatibus, earum perspiciatis? Delectus voluptatum cupiditate dignissimos impedit obcaecati labore sequi nemo, error, quibusdam, vero aperiam minima est illo consequatur hic aspernatur minus harum non asperiores corrupti expedita rem distinctio pariatur debitis? Voluptate cumque eum quas molestias, optio temporibus velit?</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, ex? Provident sed voluptates excepturi expedita libero laudantium minus accusamus, quae nisi, ipsa vel officiis ad dicta quos vitae quo delectus maiores, fugit cupiditate eum corrupti facilis? Eveniet obcaecati reprehenderit odit ipsam porro eligendi animi magni nulla cum aperiam harum cupiditate, maxime, veniam consequatur laboriosam voluptates quia! Eius distinctio sit cumque, praesentium eligendi illum dolorum quibusdam tempore qui in officia, architecto dolore minus quos. Sit repellat minus quo quas dicta odio dolores, facilis labore dignissimos quasi porro maxime? Nesciunt cumque similique dolorem maxime tenetur molestiae est inventore voluptates possimus quam aliquid dignissimos ipsum quidem, voluptatem autem tempore labore eius cum. Maxime in, illum exercitationem illo eligendi quas excepturi accusamus inventore eum? Incidunt explicabo temporibus laudantium magnam, accusantium id amet hic, facere dolores expedita ea optio quas quasi enim vel dolorum perferendis ut placeat obcaecati sit iure officia! Accusamus, amet vel animi iure optio quam possimus alias illo sapiente ab molestias repudiandae deserunt aliquam officia veritatis officiis. Architecto sapiente nihil cupiditate, minus deleniti voluptatibus, fugit ducimus veritatis error libero perferendis suscipit in, voluptas a iure eaque ipsum accusamus ad ab officia tenetur reiciendis commodi nesciunt sequi. Tempora accusamus corporis voluptate ea vero.</p>
+        </div>
+    );
+};
+
+export default About;
